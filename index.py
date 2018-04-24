1513cf45973a61396f1c15de8589a7871aefd82d
pomfy113 <pomfy113@gmail.com>
2018-04-24 10:56:48 -0700

    Cleaned up unescaped characters

--- index.py
+++ index.py
@@ -1,30 +1,44 @@
 import urllib.request
 import json
+import sys
+from html import unescape
+
 
 def API_call(MAL_id):
-    '''
-    The API call using jikan.moe instead of the official API for ease of use.
-    May change this later on.
-    '''
+    """Calls jikan.moe instead of the official MAL API for ease of use.
+    May change this later on."""
 
     API_url = "http://api.jikan.moe/anime/{}/characters_staff".format(MAL_id)
-    data = urllib.request.urlopen(API_url).read()
 
-    decoded_data = json.loads(data.decode('utf-8'))
-    return decoded_data['character']
+    try:
+        data = urllib.request.urlopen(API_url)
+    except urllib.error.URLError as e:
+        return None
+
+    result = json.loads(data.read())
+    return result['character']
+
+
+def ID_get(file):
+    """Grabs all MAL IDs from a text file."""
+    inputfile = open(file).read()
+    return inputfile.split()
+
 
 def main():
-    '''
-    Main function; meant to grab all the MAL IDs from a file, then grab
-    character data from each one. For the time being, I'll just grab it
-    from plaintext.
-    '''
+    """Perform main function."""
+    if len(sys.argv) != 3:
+        print("Format: python index.py [file with ID input] [csv output]")
+        return
 
-    chardata = API_call(23)
-    for character in chardata:
-        print(character['name'], character['role'])
+    source_id = sys.argv[1]
+    id_arr = ID_get(source_id)
 
-
+    for id in id_arr:
+        chardata = API_call(id)
+        if chardata:
+            for character in chardata:
+                print(unescape(character['name']), character['role'])
 
 if __name__== "__main__":
     main()
