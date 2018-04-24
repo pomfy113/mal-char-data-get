eb0f9f6bf9cce71fec1feff730dbf0ee286bd057
pomfy113 <pomfy113@gmail.com>
2018-04-23 22:12:46 -0700

    Functions set-up for grabbing character info

--- index.py
+++ index.py
@@ -1,10 +1,30 @@
 import urllib.request
 import json
 
-API_url = "http://api.jikan.moe/anime/1/characters_staff"
-API_content = urllib.request.urlopen(API_url)
+def API_call(MAL_id):
+    '''
+    The API call using jikan.moe instead of the official API for ease of use.
+    May change this later on.
+    '''
 
-data = API_content.read()
-decoded_data = json.loads(data.decode('utf-8'))
+    API_url = "http://api.jikan.moe/anime/{}/characters_staff".format(MAL_id)
+    data = urllib.request.urlopen(API_url).read()
 
-print(decoded_data['character'])
+    decoded_data = json.loads(data.decode('utf-8'))
+    return decoded_data['character']
+
+def main():
+    '''
+    Main function; meant to grab all the MAL IDs from a file, then grab
+    character data from each one. For the time being, I'll just grab it
+    from plaintext.
+    '''
+
+    chardata = API_call(23)
+    for character in chardata:
+        print(character['name'], character['role'])
+
+
+
+if __name__== "__main__":
+    main()
